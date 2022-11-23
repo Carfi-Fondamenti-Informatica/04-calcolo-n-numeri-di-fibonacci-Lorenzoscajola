fc9976e82b76cf48551f19a6b86cc2f5c49fe497
Lorenzoscajola <117931214+Lorenzoscajola@users.noreply.github.com>
2022-11-23 22:50:02 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,29 +2,29 @@
 using namespace std; 
 
 int main () {
-   int n;
-	int a=1;
-	int b=1;
-	int cont=0;
+int n;
+int a=1;
+int b=1;
+int cont=0;
 	
-	cin>>n;
+cin>>n;
 	
-	if(n>=2)
-	{
-		cout<<a<<endl;
-		cout<<b<<endl;
-		for(int i=n; i>2; i--)
-		{
-		cont=a+b;
-		cout<<cont<<endl;
-		a=b;
-		b=cont;
-      }
-	}
-	else
-	{
-		cout<<"Error"<<endl;
-	}
+if(n>=2)
+{
+cout<<a<<endl;
+cout<<b<<endl;
+for(int i=n; i>2; i--)
+{
+cont=a+b;
+cout<<cont<<endl;
+a=b;
+b=cont;
+}
+}
+else
+{
+cout<<"Error"<<endl;
+}
 }
 	
 	
