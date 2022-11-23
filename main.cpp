164aa2cbba4c49573965fb7a9a5257f9a382ff38
Lorenzoscajola <117931214+Lorenzoscajola@users.noreply.github.com>
2022-11-23 22:53:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,24 @@
 #include <iostream>
 using namespace std; 
-
 int main () {
 int n;
 int a=1;
 int b=1;
 int cont=0;
-	
 cin>>n;
-	
-if(n>=2)
-{
+if(n>=2){
 cout<<a<<endl;
 cout<<b<<endl;
-for(int i=n; i>2; i--)
-{
+for(int i=n; i>2; i--){
 cont=a+b;
 cout<<cont<<endl;
 a=b;
-b=cont;
+b=cont;}
 }
+else{
+cout<<"Error"<<endl;}
 }
-else
-{
-cout<<"Error"<<endl;
-}
-}
+
+
 	
 	
